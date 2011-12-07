8f5074ee26cd4c96e579cda5b2e3bf61dcf32855
John Hutchinson <johnhutch@gmail.com>
2011-12-07 15:49:34 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Familyhutch::Application.config.secret_token = '5318a9675e028bc1fa59c1b2cce2ec194516bbeb925db56ed34d960b6ace34e45e569f6465a4bc8ab61c19d482b75a77489edf83a01b581e4c0d8ce106df6a35'
