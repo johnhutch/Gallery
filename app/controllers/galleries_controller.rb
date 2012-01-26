596b527a01375a1b3ce48c54948fe087a6331d10
John Hutchinson <johnhutch@gmail.com>
2012-01-26 02:46:26 -0500

    updated gallerly create to redirect to gallery edit.

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,5 +1,9 @@
 class GalleriesController < InheritedResources::Base
   before_filter :authenticate_user!, :except => [:show, :index]
+  
+  def create
+    create!(:notice => "Gallery created! Now upload some photos.") { edit_gallery_path(@gallery) }
+  end
     
   protected
     def begin_of_association_chain
